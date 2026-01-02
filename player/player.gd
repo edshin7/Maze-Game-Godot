1a7ea0bc67e8cedf93342182b128c4a0e1685cfc
edshin7 <edshin7@gmail.com>
2026-01-01 21:28:51 -0800

    Initial commit.

---
+++ player/player.gd
@@ -0,0 +1,21 @@
+extends Area2D
+class_name Player
+
+var block_size: int = 32
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func move_player(dir: Vector2):
+	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	pass
