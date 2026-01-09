60ff272e92a19b78fbdc4a0299b1e9dcacdf85ed
edshin7 <edshin7@gmail.com>
2026-01-08 22:56:49 -0800

    Second commit for Task 5 REDONE

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var first_level_path: String = "res://levels/maze.tscn"
+var level_select_path: String = "res://UI/level_select_page.tscn"
 
 
 func _on_play_button_pressed() -> void:
@@ -9,3 +10,7 @@ func _on_play_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_levels_button_pressed() -> void:
+	get_tree().change_scene_to_file(level_select_path)
