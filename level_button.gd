6e466bd8e339323fae7afd9f948d065761f1fb28
edshin7 <edshin7@gmail.com>
2026-01-08 23:07:24 -0800

    Third commit for Task 5 REDONE

---
+++ level_button.gd
@@ -0,0 +1,14 @@
+extends Button
+class_name LevelButton
+
+@export var level_name: String = ""
+@export var level_path: String = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file(level_path)
